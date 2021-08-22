da11ede36e748f16358810122b742372af0be9db
Jesús Zúñiga Trejo <zutjmx@gmail.com>
2021-08-22 15:27:50 -0500

    Modificaciones 22/08/2021

--- src/modules/pokemon/pages/AboutPage.vue
+++ src/modules/pokemon/pages/AboutPage.vue
@@ -1,3 +1,63 @@
 <template>
     <h1>About Page</h1>
+    <hr>
+    <h2>{{nombre}}</h2>
+    <button @click="onChangeNombre">
+        Cambiar Nombre
+    </button>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            nombre: 'Jesús Zúñiga Trejo'
+        }
+    },
+    methods: {
+        onChangeNombre() {
+            this.nombre = 'Ana María Alejandre Arroyo'
+        }
+    },
+    beforeCreate() {
+        console.log('beforeCreate')
+    },
+    created() {
+        this.nombre = 'Yesmeli'
+        console.log('created')
+    },
+    beforeMount() {
+        console.log('beforeMount')
+    },
+    mounted() {
+        console.log('mounted')
+    },
+    beforeUpdate() {
+        console.log('beforeUpdate')
+    },
+    updated() {
+        console.log('updated')
+    },
+    activated() {
+        console.log('activated')
+    },
+    deactivated() {
+        console.log('deactivated')
+    },
+    beforeUnmount() {
+        console.log('beforeUnmount')
+    },
+    unmounted() {
+        console.log('unmounted')
+    },
+    errorCaptured: (err, vm, info) => {
+        console.log('errorCaptured')
+    },
+    renderTracked({key, target, type}) {
+        console.log('renderTracked')
+    },
+    renderTriggered({key, target, type}) {
+        console.log('renderTriggered')
+    },
+}
+</script>
